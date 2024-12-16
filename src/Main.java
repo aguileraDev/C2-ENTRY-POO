85696d3e04274ebe440f44655178d19cdeb4b9fd
Manuel Aguilera <102045443+aguileraDev@users.noreply.github.com>
2024-12-16 14:16:12 -0400

    init: initial commit after repository fork action

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * @author Manuel Aguilera / @aguileradev
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome assesment-3!");
+    }
+}
